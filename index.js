45b98fb250ac46f1d30c0667f2ea6eae359971b5
Tyler Garlick <tjgarlick@gmail.com>
2013-10-16 18:02:16 -0400

    reorganization

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var _ = require('lodash')
   , dbc = require('dbc.js')
+  , Cocktail = require('cocktail')
   ;
 
 function Objectify() {
 }
 
+
+Cocktail.mix(Objectify, {
+  '@exports': module
+});
+
+
 Objectify.create = function (schema, defaults) {
   var self = this;
+
   if (_.isObject(schema)) {
     dbc([typeof schema === 'object'], 'Schema type is required');
     createObjectOfType(schema.type);
@@ -24,8 +32,27 @@ Objectify.create = function (schema, defaults) {
 };
 
 function createObjectOfType(type) {
+  type = type.toLowerCase() || 'object';
   if (type === 'object')
     return {};
+  else if (type === 'boolean')
+    return false;
+  else if (type === 'string')
+    return ''
+  else if (type === 'number') {
+    return 0;
+  } else if (type === 'array') {
+    return []
+  } else if (type === 'regex') {
+    return /\*/;
+  } else if (type === 'function') {
+    return function () {
+    };
+  } else if (type === 'undefined') {
+    return undefined;
+  } else if (type === 'null') {
+    return null;
+  }
 }
 
 
