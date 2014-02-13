ee2ea014c56d4e2a508eadbbe3816e7907f45322
Tyler Garlick <tjgarlick@gmail.com>
2014-02-13 07:27:55 -0500

    getting setup again

---
+++ test/objectify.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('expect.js')
+    ;
+
+describe('failing test', function () {
+    it('should fail', function () {
+        expect(true).to.be.ok();
+    });
+});
