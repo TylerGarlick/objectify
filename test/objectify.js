e0b8eae18005010fa2fa7bbc9fc75d280b71fe24
Tyler Garlick <tjgarlick@gmail.com>
2014-02-14 17:21:57 -0500

    fixes

--- test/objectify.js
+++ test/objectify.js
@@ -1,59 +1,66 @@
 'use strict';
 
 var expect = require('expect.js'),
-    objectify = require('../'),
-    Types = objectify.Types,
-    version = require('../package.json').version;
+  objectify = require('../'),
+  Types = objectify.Types,
+  version = require('../package.json').version;
+
 
 describe('Objectify', function () {
 
-    beforeEach(function () {
-        expect(objectify).to.be.ok();
+  beforeEach(function () {
+    expect(objectify).to.be.ok();
+  });
+
+  describe('config', function () {
+    describe('properties', function () {
+      describe('#version', function () {
+        it('should have the same version as the package', function () {
+          expect(objectify.config.version).to.eql(version);
+        });
+      });
+
+      describe('#ioc', function () {
+        it('should have an ioc container by default', function () {
+          expect(objectify.config.ioc).to.be.ok();
+        });
+      });
+    });
+  });
+
+  describe('Types', function () {
+    describe('Stereotype', function () {
+      it('should exists as a prototypal function', function () {
+        expect(Types.Stereotype).to.be.a.func;
+        var kind = new Types.Stereotype({});
+        expect(kind).to.be.ok();
+        expect(kind).to.be.a(Types.Stereotype);
+      });
     });
 
-    describe('config', function () {
-        describe('properties', function () {
-            describe('#version', function () {
-                it('should have the same version as the package', function () {
-                    expect(objectify.config.version).to.eql(version);
-                });
-            });
-        });
-    })
+    describe('ObjectMetadata', function () {
+      it('should exists as a prototypal function', function () {
+        expect(Types.ObjectMetadata).to.be.a.func;
+        var metadata = new Types.ObjectMetadata('special', {});
+        expect(metadata).to.be.ok();
+        expect(metadata).to.be.a(Types.ObjectMetadata);
+      });
 
-    describe('Types', function () {
-        describe('Stereotype', function () {
-            it('should exists as a prototypal function', function () {
-                expect(Types.Stereotype).to.be.a.func;
-                var kind = new Types.Stereotype({});
-                expect(kind).to.be.ok();
-                expect(kind).to.be.a(Types.Stereotype);
-            });
-        });
+      it('should require a name', function () {
+        expect(function () {
+          new Types.ObjectMetadata();
+        }).to.throwError;
+      });
 
-        describe('ObjectMetadata', function () {
-            it('should exists as a prototypal function', function () {
-                expect(Types.ObjectMetadata).to.be.a.func;
-                var metadata = new Types.ObjectMetadata('special', {});
-                expect(metadata).to.be.ok();
-                expect(metadata).to.be.a(Types.ObjectMetadata);
-            });
+      it('should make the name the type formatted', function () {
+        var metadata = new Types.ObjectMetadata("MeSpecialType");
+        expect(metadata).to.be.ok();
+        console.log(metadata.type);
+        expect(metadata.type).to.equal('meSpecialType');
 
-            it('should require a name', function () {
-                expect(function () {
-                    new Types.ObjectMetadata();
-                }).to.throwError;
-            });
-
-            it('should make the name the type formatted', function () {
-                var metadata = new Types.ObjectMetadata("MeSpecialType");
-                expect(metadata).to.be.ok();
-                console.log(metadata.type);
-                expect(metadata.type).to.equal('meSpecialType');
-
-            });
-        });
+      });
     });
+  });
 
 
 });
--- test/validator.js
+++ test/validator.js
@@ -1,21 +1,21 @@
 'use strict';
 
 var expect = require('expect.js'),
-    objectify = require('../'),
-    validator = objectify.config.validator;
+  objectify = require('../'),
+  validator = objectify.config.validator;
 
 describe('Validator', function () {
-    beforeEach(function () {
-        expect(objectify).to.be.ok();
-        expect(validator).to.be.ok();
-    });
+  beforeEach(function () {
+    expect(objectify).to.be.ok();
+    expect(validator).to.be.ok();
+  });
 
-    describe("methods", function () {
-        describe("#validate()", function () {
-            it('should have a validate function', function () {
-                expect(validator.validate).to.be.ok();
-                expect(validator.validate).to.be.a.func;
-            });
-        });
+  describe("methods", function () {
+    describe("#validate()", function () {
+      it('should have a validate function', function () {
+        expect(validator.validate).to.be.ok();
+        expect(validator.validate).to.be.a.func;
+      });
     });
+  });
 });
